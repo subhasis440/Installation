b3f07173e8f5749afb7566c8ed211f151c97a427
root <root@amazonlinux.onprem>
2024-04-16 09:15:21 +0000

    add file

--- k8nodesetupcentos.sh
+++ k8nodesetupcentos.sh
@@ -76,6 +76,7 @@ sudo firewall-cmd --permanent --add-port=6443/tcp
 sudo firewall-cmd --permanent --add-port=10250/tcp
 sudo firewall-cmd --reload
 sudo yum install -y conntrack-tools
+rm -rf install_kubadm_kublet.sh
 
 # Verify installations
 kubectl version --client
