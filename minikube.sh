21d1e72ca78a29822d6271926538b36e03cbb6e8
Subhasis Sahoo <subhasis.0891@gmail.com>
2024-05-29 21:10:19 +0530

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,51 @@
+sudo apt-get update
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/' /etc/fstab
+sudo mkdir -m 755 /etc/apt/keyrings
+
+sudo apt-get install -y docker.io
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo apt-get install -y vim git curl wget apt-transport-https ca-certificates curl gnupg conntrack
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+sudo apt update -y
+sudo apt-get -y install containerd
+
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.14/cri-dockerd_0.3.14.3-0.ubuntu-jammy_amd64.deb
+sudo dpkg -i cri-dockerd_0.3.14.3-0.ubuntu-jammy_amd64.deb
+cri-dockerd --version
+
+minikube start --driver=none
